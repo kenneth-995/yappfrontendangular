de7e8fb22e9e760d2f3c1403e62b5e0f70c62b27
Kenneth griñan <KENNETH.FERRE@GMAIL.COM>
2021-04-27 22:00:27 +0200

    sent request login backend

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Subject } from 'rxjs';
 
 import { UserLoginDto } from '../models/dto/UserLoginDto';
@@ -14,10 +15,30 @@ export class UserService {
 
   login(formData: UserLoginDto) {
 
-    let headers = {'Content-Type':'application/json'};
+    //let headers = {'Content-Type':'application/json'};
     // 'Access-Control-Allow-Origin:': '*'
 
-    return this.htttClient.post('http://localhost:5000/login', formData)
+    let headers = new Headers();
+    headers.append('Accept', 'application/json')
+    // creating base64 encoded String from user name and password
+    var base64Credential: string = btoa( formData.username+ ':' + formData.password);
+    headers.append("Authorization", "Basic " + base64Credential);
+
+
+    const headerDict = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Authorization': "Basic " + base64Credential,
+    }
+    
+
+    
+    const requestOptions = {                                                                                                                                                                                 
+      headers: new HttpHeaders(headerDict), 
+    };
+
+    return this.htttClient.get('http://localhost:5000/login', requestOptions);
 
     //return this.htttClient.get('http://localhost:5000/api/family/')
 
