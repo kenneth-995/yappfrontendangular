5d8b253127173b3f712c90928ebffa6e289da542
kenneth Griñan <kennethgrinan@gmail.com>
2021-05-10 22:51:15 +0200

    create page not found

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NoPageFoundComponent],
   imports: [
     CommonModule
   ],
