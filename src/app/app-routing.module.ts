61ec91ec148804d18a6139209c361d0ad1afdea5
kenneth Griñan <kennethgrinan@gmail.com>
2021-05-10 23:01:48 +0200

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { 
@@ -13,7 +14,7 @@ const routes: Routes = [
   },
   { 
     path: 'home', 
-    //canActivate: [ControlCanViewComponent],
+    canActivate: [AuthGuard],
     loadChildren: () => 
     import('src/app/modules/main/main.module').then(m => m.MainModule) 
   }
