9acd61aa562f3194dfdba0165e85ae20b110400a
kenneth Griñan <kennethgrinan@gmail.com>
2021-05-15 15:24:33 +0200

    package.json

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/YAPPFRONTEND'));
+app.use(express.static(__dirname + '/dist/yappfrontend'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/YAPPFRONTEND/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/yappfrontend/index.html'));
 });
 
 // default Heroku port
-console.log('hello! in port ' + process.env.PORT)
+console.log('hello! in PORT ' + process.env.PORT)
+console.log('hello! in PATH ' + path)
 app.listen(process.env.PORT || 5000);
