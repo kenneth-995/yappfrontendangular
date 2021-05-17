dbf5bfb41ea4c18aec67dd7df1cb7fbb2e733e4c
asd <kennethgrinan@gmail.com>
2021-05-17 19:06:39 +0200

    todo: add console.log when token is expired

--- src/app/core/interceptors/auth.service.ts
+++ src/app/core/interceptors/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, Http
 import { Observable, Subject, throwError } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
@@ -11,7 +12,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class AuthService implements HttpInterceptor {
   count: number = 0;
 
-  constructor(private route: Router, private spinner : NgxSpinnerService) {}
+  constructor(private route: Router, private spinner : NgxSpinnerService, private toast: ToastrService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.spinner.show();
@@ -32,7 +33,14 @@ export class AuthService implements HttpInterceptor {
     
 
     return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
+      console.log('[AuthService.ts]');
       console.log(error);
+      console.log(error.status);
+      console.log(error.message);
+      console.log('[AuthService token = ]'+token)
+      console.log()
+      console.log(request)
+      console.log('end [AuthService.ts]');
       if (error.status == 401) { //403
         if (request.url.includes('info') == false) {
           //this.ngxLoader.stop();
@@ -40,6 +48,20 @@ export class AuthService implements HttpInterceptor {
           localStorage.removeItem('username');
           this.route.navigateByUrl('/login')
         }
+      };
+
+
+      if (error.status == 500) { //TODO: refresh token, add endpoint refresh  backend
+        console.log('STATUS 500')
+        //TODO: MANAGE REFRESH TOKEN IN BACKEND
+        this.route.navigateByUrl('/login')
+        console.log('PLEASE, RENEW TOKEN , DEVES VOLVERTE A LOGUEAR')
+        this.toast.warning('Please re-enter the login form', 'INFO')
+        //this.ngxLoader.stop();
+        /*localStorage.removeItem('accessToken');
+        localStorage.removeItem('username');
+         */
+
       };
       
       return next.handle(request)
