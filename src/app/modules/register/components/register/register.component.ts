7379d3d334a0a507829ca15a9f3a73a16115acea
kenneth Griñan <kennethgrinan@gmail.com>
2021-05-21 13:32:31 +0200

    delete form register data

--- src/app/modules/register/components/register/register.component.ts
+++ src/app/modules/register/components/register/register.component.ts
@@ -38,7 +38,20 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
 
     this.registerForm = this.fb.group({
-      username: ['kenneth', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+      name: ['', Validators.required],
+      surnames: ['', Validators.required],
+      phone: ['', Validators.required],
+      collegiateNumber: ['', Validators.required],
+      specialistType: ['', Validators.required],
+      nameClinic: ['', Validators.required],
+      addressClinic: ['', Validators.required],
+      phoneNumberClinic: ['', Validators.required],
+      emailClinic: ['', Validators.required]
+      /* username: ['kenneth', Validators.required],
       email: ['kenneth.ferre@gmail.com', Validators.required],
       password: ['password', Validators.required],
       password2: ['passwordd', Validators.required],
@@ -50,7 +63,7 @@ export class RegisterComponent implements OnInit {
       nameClinic: ['Super Clinic', Validators.required],
       addressClinic: ['C/Moli de la torre', Validators.required],
       phoneNumberClinic: ['650190006', Validators.required],
-      emailClinic: ['kenneth.ferre@gmail.com', Validators.required]
+      emailClinic: ['kenneth.ferre@gmail.com', Validators.required] */
     });
 
     this.inicializeForm();
